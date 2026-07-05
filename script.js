0cf0f7c0455d3d92e4ac1f219ee95507e1fe5048
allanpetergiffy <wallanpetergiffy@gmail.com>
2026-07-05 13:44:03 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+/**
+ * Triggers the printing system for the layout block element grid.
+ * Safely targets the printing layout viewport natively.
+ */
+function printCards() {
+    window.print();
+}
